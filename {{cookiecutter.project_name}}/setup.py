939c0e0efb6dc4b3fd2ca0fe55118ad26fae907b
David Wolfe <dave.wolfe@blue-newt.com>
2015-12-02 15:45:02 -0500

    Kill `test_requirements.txt` use pytest-runner.

--- {{cookiecutter.project_name}}/setup.py
+++ {{cookiecutter.project_name}}/setup.py
@@ -6,16 +6,10 @@ from setuptools import setup, find_packages
 
 readme = open('README.md').read()
 
-# NB: _don't_ add namespace_packages to setup(), it'll break
-#     everything using imp.find_module
 setup(
-    author='{{ cookiecutter.full_name }}',
-    author_email='{{ cookiecutter.email }}',
+    author='Blue Newt Software',
+    author_email='support@blue-newt.com',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.7',
     ],
@@ -29,11 +23,16 @@ setup(
     long_description=readme,
     name='{{ cookiecutter.package_name }}',
     namespace_packages=['{{ cookiecutter.package_name.split(".")[0] }}' ],
-    packages=[
-        '{{ cookiecutter.package_name }}',
+    packages=find_packages(),
+    setup_requires=[
+        'pytest-runner',
+        'setuptools_scm'
     ],
-    setup_requires=['setuptools_scm'],
     test_suite='tests',
+    tests_require=[
+        'coverage',
+        'pytest',
+    ],
     url='{{ cookiecutter.vcs_url }}',
     use_scm_version=True,
     zip_safe=False,
