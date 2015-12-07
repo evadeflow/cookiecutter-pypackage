c9078a28cec8916fdc4cad7f9aed803a94b98548
David Wolfe <dave.wolfe@blue-newt.com>
2015-12-07 14:22:41 -0500

    Use callable _object_ (not function) as `use_scm_version` arg.

--- {{cookiecutter.project_name}}/setup.py
+++ {{cookiecutter.project_name}}/setup.py
@@ -6,35 +6,40 @@ from setuptools import setup, find_packages
 
 readme = open('README.md').read()
 
-# See: https://pypi.python.org/pypi/pytest-runner#conditional-requirement
+# Skip pytest-runner install if not testing, see:
+#   https://pypi.python.org/pypi/pytest-runner#conditional-requirement
 needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
-pytest_runner = ['pytest-runner'] if needs_pytest else []
+pytest_runner = ['pytest-runner>=2.0,<3dev'] if needs_pytest else []
 
 # Temp hack to work around the lack of an SCM working copy in our current
 # deploys: just return a hard-coded, dummy value if the lookup via SCM tags
 # would fail.
 dummy_version = '0.1.0'
-def get_use_scm_version_arg():
+class GetUseScmVersionArg():
 
-    def dummy_get_version(scm_path):
+    def dummy_version_from_scm(self, scm_root):
         return dummy_version
 
-    import setuptools_scm
+    def __call__(self, *args, **kwds):
+        import setuptools_scm
 
-    scm_version_okay = False
-    for scm_path in ('.', '..'):
-        try:
-            setuptools_scm.get_version(scm_path)
-            scm_version_okay = True
-            break
-        except LookupError:
-            pass
+        scm_version_okay = False
+        for scm_path in ('.', '..'):
+            try:
+                setuptools_scm.get_version(scm_path)
+                scm_version_okay = True
+                break
+            except LookupError:
+                pass
 
-    if not scm_version_okay:
-        # Monkey patch the internal setuptools_scm function version_from_scm()
-        setattr(setuptools_scm, 'version_from_scm', dummy_get_version)
+        if not scm_version_okay:
+            # Monkey patch the internal setuptools_scm function version_from_scm()
+            setattr(setuptools_scm, 'version_from_scm', self.dummy_version_from_scm)
 
-    return { 'root': scm_path, 'version_scheme': 'post-release' }
+        return { 'root': scm_path, 'version_scheme': 'post-release' }
+
+    def pop(self, value, retval=None):
+        return retval
 
 setup(
     author='Blue Newt Software',
@@ -61,7 +66,7 @@ setup(
         'coverage',
         'pytest',
     ],
-    use_scm_version=get_use_scm_version_arg,
+    use_scm_version=GetUseScmVersionArg(),
     version=dummy_version,
     zip_safe=False,
 )
